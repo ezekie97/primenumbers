6dd32cc365836951e77118cb65d7f3b38063f8e7
ezekie97 <ezekie97@users.noreply.github.com>
2014-11-13 12:53:49 -0500

    Update primeClient.py

--- primeClient.py
+++ primeClient.py
@@ -3,11 +3,16 @@ import rpyc
 class primesClient:
     """ establish connection"""
     def __init__(self):
-        self.c = rpyc.connect("10.0.2.15",12345)
+        print("Connecting")
+        self.c = rpyc.connect("150.250.211.136",12345)
+        print("Connected")
 
     """ reach to primes service"""
     def getPrimesService(self):
         num = int(input("Please type a number: "))
         return self.c.root.findPrimes(num)
+client = primesClient()
+client.getPrimeService()
     
+
     
